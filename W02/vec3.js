0a0078aa5070682c4324fd0cc80f61aa01349c5c
岡田　海功 <172x003x@istc-a-065.epc.center.kobe-u.ac.jp>
2017-04-12 14:55:16 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,7 +1,23 @@
 //Constructor
-Vec3= function(x,y,z)
+Vec3 = function(x,y,z)
 {
     this.x = x;
     this.y = y;
     this.z = z;
+}
+
+//Add method
+Vec3.prototype.add = function(v)
+{
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+
+    return this;
+}
+
+//Sum method
+Vec3.prototype.sum = function()
+{
+    return this.x + this.y + this.z;
 }
