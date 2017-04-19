92e0621671f9501c963c566e089aaabc5318713d
岡田　海功 <172x003x@istc-a-046.epc.center.kobe-u.ac.jp>
2017-04-19 13:58:17 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -21,3 +21,38 @@ Vec3.prototype.sum = function()
 {
     return this.x + this.y + this.z;
 }
+
+//min method
+Vec3.prototype.min = function()
+{
+    var min = this.x;
+    if(min > this.y)
+    {
+	min = this.y;
+    }
+    if(min > this.z)
+    {
+	min = this.z;
+    }
+
+    return min;
+}
+
+//max method
+Vec3.prototype.max = function()
+{
+    var max = this.x;
+    if(max < this.y)
+    {
+	max = this.y;
+    }
+    if(max < this.z)
+    {
+	max = this.z;
+    }
+
+    return max;
+}
+
+//mid method
+Vec3.prototype.max = function()
